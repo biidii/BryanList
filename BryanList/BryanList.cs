3bca0e82feb276c32906691ba4c7041801a4cb93
Edgar Torres Roldan <torresedgar@yahoo.com>
2017-05-01 22:39:36 -0700

    Initial commit

---
+++ BryanList/BryanList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BryanList
+{
+    public class BryanList
+    {
+        public void Add();
+
+        public void Remove();
+
+        public void Count();
+
+        public void GetIndex(int index);
+
+        public void SetIndex(int value);
+
+        public void PrintOutList();
+
+        public void Copy(BryanList list);
+
+        public void Clear();
+    }
+}
---
+++ BryanList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BryanList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
